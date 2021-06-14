e9796ffa4fac78c98ca4db23ab4359fb59fe596f
Aryan-Kasat <72985192+Aryan-Kasat@users.noreply.github.com>
2021-06-14 19:46:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,16 +9,36 @@ Recognition.onresult=function(event){
     var content=event.results[0][0].transcript;
     console.log(content)
     document.getElementById("textbox").innerHTML=content;
-    speak();
+    if(content=="take my selfie"){
+        speak();
+
+    }
+    
 }
 function speak(){
     var synthesis=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="taking your selfie in five seconds";
     var utter=new SpeechSynthesisUtterance(speak_data);
     synthesis.speak(utter);
     Webcam.attach(camera);
+    setTimeout(function (){
+        takesnapshot();
+        save();
+    },5000);
    }
    Webcam.set({
    width:350,height:250,image_format:'png',png_quality:100
    });
    camera=document.getElementById("camera");
+   function takesnapshot(){
+       Webcam.snap(function(data_url){
+           document.getElementById("result").innerHTML="<img id='new_image' src='"+data_url+"'>";
+
+       });
+   }
+   function save(){
+       link=document.getElementById("link");
+       img=document.getElementById("new_image").src;
+       link.href=img;
+       link.click();
+   }
